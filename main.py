7f112caacf39bd2ef8dae8f47f3274ac89bbb62b
wannacry3030 <wannacry.bat@proton.me>
2024-04-04 17:49:39 -0300

    update main.py

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import tkinter as tk
 
+#funcao que calcula o custo
 def calcular_custo():
-    valor_por_palavra = float(valor_por_palavra_entry.get())
-    total_palavras = int(total_palavras_entry.get())
-    custo_total = (valor_por_palavra * total_palavras) / 100  # Convertendo para reais
-    custo_label.config(text=f"Custo Total: R$ {custo_total:.2f}")
-
-# Configuração da janela
+    valorPorPalavra=float(valorPorPalavra_entry.get())
+    totalPalavras=int(totalPalavras_entry.get())
+    custoTotal=(valorPorPalavra*totalPalavras)/100
+    custo_label.config(text=f"Custo Total: R$ {custoTotal:.2f}")
+    
+#criando a GUI
 root = tk.Tk()
-root.title("Calculadora de Custo por Palavra")
+root.title("Calc Custo por Palavra")
 
-# Layout
-tk.Label(root, text="Valor por palavra (centavos):").pack()
-valor_por_palavra_entry = tk.Entry(root)
-valor_por_palavra_entry.pack()
+#criando os widgets
+tk.Label(root,text="Valor por palavra (centavos):").pack()
+valorPorPalavra_entry = tk.Entry(root)
+valorPorPalavra_entry.pack()
 
-tk.Label(root, text="Total de palavras:").pack()
-total_palavras_entry = tk.Entry(root)
-total_palavras_entry.pack()
+tk.Label(root, text="Total de Palavra:").pack()
+totalPalavras_entry =tk.Entry(root)
+totalPalavras_entry.pack()
 
 tk.Button(root, text="Calcular", command=calcular_custo).pack()
 
-custo_label = tk.Label(root, text="Custo Total: R$ 0.00")
+#exibindo o resultado
+custo_label=tk.Label(root, text="Custo Total: R$ 0.00")
 custo_label.pack()
 
-# Iniciando a aplicação
+#iniciando o loop
 root.mainloop()
